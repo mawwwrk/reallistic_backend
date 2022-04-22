35be6689e4baef5d84768da118af0b220ef067f0
Mork <mawwwrk@gmail.com>
2022-04-22 09:21:08 +0800

    minor changes, to be pushed to upstream.
    made mongodb connection message more informative
    changed flag from 'isproduction' to 'uselocal'
    for slightly easier switcing of contexts
    working on auth to ensure only successfull logins are successful

--- server.js
+++ server.js
@@ -6,10 +6,13 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 const ListingController = require("./controllers/listingController");
-const AuthController = require("./controllers/auth");3
+const AuthController = require("./controllers/auth");
+3;
 const tenantUserController = require("./controllers/tenantUserController");
 const morgan = require("morgan");
 const { isProduction, mongoUri, prodUrls, ports } = require("./util/envhelper");
+const { User } = require("./models/Users");
+const Listing = require("./models/Listing");
 const morganFmt = isProduction ? "tiny" : "dev";
 const frontEndUrls = [
   `http://localhost:${ports.local.frontend}`,
@@ -25,7 +28,9 @@ mongoose.connection
   .on("error", (err) => console.log(err.message + " is Mongod not running?"))
   .on("disconnected", () => console.log("mongo disconnected"))
   .once("open", () => {
-    console.log("connected to mongoose...");
+    console.log(
+      "connected to mongoose at \033[94,1m" + `${MONGODB_URI}` + "\033[0m"
+    );
   });
 
 mongoose.connect(MONGODB_URI, {
@@ -47,8 +52,16 @@ app
   .use("/api/listings", ListingController)
   .use("/auth", AuthController);
 
-app.get("/", (req, res) => {
-  res.send("Welcome to Reallistic");
+app.get("/", async (req, res) => {
+  const db = await mongoose.connection.asPromise();
+  console.log("db", db);
+  // const users = await User.find().exec();
+  // const listings = await Listing.find().exec();
+  // .then((res) => console.log("res", res))
+  // .catch((err) => console.log("err", err));
+
+  res.send({ users, listings });
+  // res.send("Welcome to Reallistic");
 });
 
 app.use("/api/tenant", tenantUserController);
